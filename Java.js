672a562d787a6847f95c885db92a52f77352a807
timotherenard <timo.renard@hotmail.fr>
2023-08-17 13:10:13 +0400

    responsive amelioration

--- Java.js
+++ Java.js
@@ -1,1 +1,17 @@
 
+
+
+function responsivemenu(){
+	if (document.getElementById("colonnegauche").className=="left-column navmenu-hidden"){
+		document.getElementById("colonnegauche").className="left-column navmenu-visible";
+	}
+	else{
+		document.getElementById("colonnegauche").className="left-column navmenu-hidden";
+	}	
+	
+}
+
+
+function menuoff(){
+	document.getElementById("colonnegauche").className="left-column navmenu-hidden";
+}
